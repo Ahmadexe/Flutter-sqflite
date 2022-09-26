bf16fa9a291f7d0f6405dc5b9687edd93c5e4a0e
Ahmad <muahmad710@gmail.com>
2022-09-26 16:42:03 +0500

    Delete added

--- lib/database/local_database_helper.dart
+++ lib/database/local_database_helper.dart
@@ -49,7 +49,11 @@ class DatabaseHelper {
   Future<int> update(Map<String, dynamic> row) async {
     Database? db = await instance.database;
     int id = row[_columnId];
-    return db!.update(_tableName, row, where: '$_columnId = ? $_columnUser = ?', whereArgs: [1, 'Ahmad']);
-
+    return await db!.update(_tableName, row, where: '$_columnId = ?', whereArgs: [id]);
   }
-}
+
+  Future<int> delete(int id) async {
+    Database? db = await instance.database;
+    return await db!.delete(_tableName, where: '$_columnId = ?', whereArgs: [id]);
+  }
+} 
